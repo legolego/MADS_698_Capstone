b171192381a8395f2c0130e56e30bb201ee67d14
Oleg Nikolsky <nikolsky@umich.edu>
2022-04-08 02:38:11 +0000

    Initial commit

---
+++ streamlit/next_big_thing.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# import import_ipynb
+# import find_category_from_thing_final as fc
+# from nbt_utils import hello, graph_sent, get_wikipedia_search_results
+
+
+st.title('Next Big Thing')
+st.markdown("""Find the next big thing!""")
+
+nbt_input = st.text_input('Find the next big thing like...', 'type here')
+
+# list_of_choices = get_wikipedia_search_results(nbt_input)
+# list_of_choices.insert(0, 'Select one')
+#print(list_of_choices)
+
+
+# if nbt_input not in ['type here', '']:
+
+#     wiki_selection = st.selectbox(
+#     'Can you please narrow down your query?',
+#     list_of_choices)
+
+#     st.write('You selected:', wiki_selection)
+
+#     if wiki_selection != 'Select one':
+#         #Get category
+#         category = fc.get_category_from_search_term(wiki_selection)
+#         #print(category)
+
+#         #Get subreddits
+
+#         #Get influencers
+
+#         #Get relevant influencers posts
+
+#         #Extract relevant phrases to category
+
+#         #Select final answer
+
+#         #nbt = 'next big thing'
+#         nbt = category[0]
+
+#         length = len(nbt_input)
+
+#         st.write('"',' '.join(category),'"', ' is the category of the next big:', wiki_selection, length)
