a5a5a446133d8fa8c24886bb540bb01ad1578412
Oleg Nikolsky <nikolsky@umich.edu>
2022-04-08 03:28:23 +0000

    attempting some libraries

--- streamlit/next_big_thing.py
+++ streamlit/next_big_thing.py
@@ -12,37 +12,37 @@ st.markdown("""Find the next big thing!""")
 
 nbt_input = st.text_input('Find the next big thing like...', 'type here')
 
-# list_of_choices = get_wikipedia_search_results(nbt_input)
-# list_of_choices.insert(0, 'Select one')
-#print(list_of_choices)
+list_of_choices = get_wikipedia_search_results(nbt_input)
+list_of_choices.insert(0, 'Select one')
+print(list_of_choices)
 
 
-# if nbt_input not in ['type here', '']:
+if nbt_input not in ['type here', '']:
 
-#     wiki_selection = st.selectbox(
-#     'Can you please narrow down your query?',
-#     list_of_choices)
+    wiki_selection = st.selectbox(
+    'Can you please narrow down your query?',
+    list_of_choices)
 
-#     st.write('You selected:', wiki_selection)
+    st.write('You selected:', wiki_selection)
 
-#     if wiki_selection != 'Select one':
-#         #Get category
-#         category = fc.get_category_from_search_term(wiki_selection)
-#         #print(category)
+    if wiki_selection != 'Select one':
+        #Get category
+        category = fc.get_category_from_search_term(wiki_selection)
+        #print(category)
 
-#         #Get subreddits
+        #Get subreddits
 
-#         #Get influencers
+        #Get influencers
 
-#         #Get relevant influencers posts
+        #Get relevant influencers posts
 
-#         #Extract relevant phrases to category
+        #Extract relevant phrases to category
 
-#         #Select final answer
+        #Select final answer
 
-#         #nbt = 'next big thing'
-#         nbt = category[0]
+        #nbt = 'next big thing'
+        nbt = category[0]
 
-#         length = len(nbt_input)
+        length = len(nbt_input)
 
-#         st.write('"',' '.join(category),'"', ' is the category of the next big:', wiki_selection, length)
+        st.write('"',' '.join(category),'"', ' is the category of the next big:', wiki_selection, length)
