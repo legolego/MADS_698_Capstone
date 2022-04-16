1d9cc07c1a29ba2720057310d2d7490f242ae70e
cl-crow <clcrow@umich.edu>
2022-04-16 18:21:21 +0000

    test app update

--- streamlit/next_big_thing.py
+++ streamlit/next_big_thing.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 #get_stanza_dict_of_first_sentence, 
 
-from nbt_utils import *
+from nbt_utils import get_mvp_terms
 
 # https://stackoverflow.com/questions/69768380/share-streamlit-cant-find-pkl-file
 img_path = Path(__file__).parents[1] / 'streamlit/images/NextBigThingHeader.png'
@@ -27,7 +27,7 @@ st.image(image, caption='The Thing vs The Thing vs Thing')
 #st.markdown("""Find the next big thing!!!""")
 if st.button('Model'):
      st.header('App Mode')
- else:
+else:
     st.header("Introduction")
 
     st.header("Step 1: Find Category from Thing")
@@ -97,6 +97,6 @@ if st.button('Model'):
      
         #st.graphviz_chart(graph_sent(get_stanza_dict_of_first_sentence(wiki_page.summary)), use_container_width=False)
 
-st.write("Being unfamiliar with Reddit prior to this project, we were pretty shocked to realize the depth and reach of Reddit when considering using it for our master’s Capstone project. According to statistics from 2020 found in 10 Reddit Statistics Every Marketer Should Know, there are more than 52 million daily active Reddit users worldwide, many of whom are from the United States. In the US there are nearly 222 million monthly active users and 18% of American adults say they are Reddit users. What differentiates Reddit from other social media networks is its structure around communities, known as subreddits. All conversations on Reddit take place within a community with the intention that all posts and comments focus on the specific topic(s) that the subreddit was created for. This means that if you look at the activity within a subreddit, you can safely assume that it will focus on the content you care about. Reddit has moderators within each subreddit that enforce community rules, such as the type of posts that are and are not allowed, including staying on topic.")
+#st.write("Being unfamiliar with Reddit prior to this project, we were pretty shocked to realize the depth and reach of Reddit when considering using it for our master’s Capstone project. According to statistics from 2020 found in 10 Reddit Statistics Every Marketer Should Know, there are more than 52 million daily active Reddit users worldwide, many of whom are from the United States. In the US there are nearly 222 million monthly active users and 18% of American adults say they are Reddit users. What differentiates Reddit from other social media networks is its structure around communities, known as subreddits. All conversations on Reddit take place within a community with the intention that all posts and comments focus on the specific topic(s) that the subreddit was created for. This means that if you look at the activity within a subreddit, you can safely assume that it will focus on the content you care about. Reddit has moderators within each subreddit that enforce community rules, such as the type of posts that are and are not allowed, including staying on topic.")
 
-st.write("CRF trained on whole corpus, not indiviudual wiki categories as some are too small, one a few articles")
+#st.write("CRF trained on whole corpus, not indiviudual wiki categories as some are too small, one a few articles")
